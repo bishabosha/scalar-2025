bf9657266583ee26477bb17c675c025cd02c4363
Jamie Thompson <bishbashboshjt@gmail.com>
2025-02-08 16:49:14 +0100

    update plan - bamboozle the audience with many short demos and shout out this repo to develop them further

--- nt.scala
+++ nt.scala
@@ -30,12 +30,59 @@ let's explore DSL's for config, data, and scripting, for a more dynamic feel.
 
 /* === Research work ===
 1. revised ops-mirror based on named tuples
-2. typed schema derivation from naked named-tuples
+2. encoding of trees in an array?
+3. basically data frame - SArray[(x: Int, y: Int)] => (Array[Int], Array[Int])
+3. off-heap structs?
+4. typed schema derivation from naked named-tuples
+5. can I hava a basic json dsl for use with sttp?
+  a. with extraction of fields? (like a cursor)
+  b. pattern matching?
+  c. can I implement a simple encoder/decoder?
+6. can I get json working with ops-mirror?
  */
 
-// SArray[(x: Int, y: Int)]
-// =>
-// (Array[Int], Array[Int])
+// type NamedTuple[N <: Tuple, T <: Tuple]
+// import scala.language.strictEquality
+import scala.NamedTuple.NamedTuple
+import scala.NamedTuple.AnyNamedTuple
+import scala.NamedTuple.Empty
+import stringmatching.regex.Interpolators.r
+
+class Config[T <: AnyNamedTuple]
+
+// TODO: turn this into a Config[T] type (so JSON like DSL that can be parsed to data)
+val config = (
+  cls = (
+    name = "ModuleKind",
+    sum = true,
+    modifier = "private",
+    ctor = List(
+      "NoModule" -> "NoModule.type",
+      "CommonJSModule" -> "CommonJSModule.type",
+      "ESModule" -> "ESModule.type"
+    ),
+    values = Nil
+  ),
+  padding = 4
+)
+// derived values
+
+val configX = Config[
+  (
+      cls: (
+          name: "ModuleKind",
+          sum: true,
+          modifier: "private",
+          ctor: (
+              NoModule: "NoModule.type",
+              CommonJSModule: "CommonJSModule.type",
+              ESModule: "ESModule.type"
+          ),
+          values: Empty
+      ),
+      padding: 4
+  )
+]
 
 val a =
   (
--- project.scala
+++ project.scala
@@ -1,2 +1,3 @@
 //> using option -language:experimental.namedTuples
 //> using scala 3.6.3
+//> using dep io.github.bishabosha::enhanced-string-interpolator::1.0.2
