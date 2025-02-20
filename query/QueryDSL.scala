dac15c503dffb8d31291a1b26cd37bac140ee514
Jamie Thompson <bishbashboshjt@gmail.com>
2025-02-20 19:00:57 +0100

    add interpreter

--- query/QueryDSL.scala
+++ query/QueryDSL.scala
@@ -2,26 +2,64 @@ package ntquery
 
 // NOTE STRIPPED BACK FOR BASIC USE CASE ONLY
 
-import NamedTuple.{NamedTuple, AnyNamedTuple, Names}
+import NamedTuple.{NamedTuple, AnyNamedTuple, Names, DropNames}
 import Tuple.IsMappedBy
 import Utils.*
 import ntquery.DeleteQuery.Filtered
 
-trait Table[T] extends Product:
+trait Table[T: TableSchema] extends Product:
+  final def schema: TableSchema[T] = summon[TableSchema[T]]
   final def name: String = productPrefix
-  final class RefId()
   final def select: SelectQuery[T, false] = SelectQuery.SelectFrom(this)
   final def delete: DeleteQuery[T] = DeleteQuery.DeleteFrom(this)
   final def insert: InsertQuery[T] = InsertQuery.InsertTo(this)
 
+sealed trait TableSchema[T]:
+  def names: IArray[String]
+  def fields: IArray[ColSchema[?]]
+  def field(name: String): Option[ColSchema[?]]
+  def construct(p: Product): T
+
+object TableSchema:
+  final class NTTableSchema[T](ns: Tuple, fs: Tuple, f: Product => T) extends TableSchema[T]:
+    val names: IArray[String] = ns.toIArray.map(_.asInstanceOf[String])
+    val fields: IArray[ColSchema[?]] = fs.toIArray.map(_.asInstanceOf[ColSchema[?]])
+    private val lookup: Map[String, ColSchema[?]] = names.iterator.zip(fields.iterator).toMap
+    def field(name: String): Option[ColSchema[?]] = lookup.get(name)
+    def construct(p: Product): T = f(p)
+
+  transparent inline given [T]: TableSchema[T] =
+    NTTableSchema[T](
+      compiletime.constValueTuple[Names[NamedTuple.From[T]]],
+      compiletime.summonAll[Tuple.Map[DropNames[NamedTuple.From[T]], ColSchema]],
+      compiletime.summonFrom {
+        case m: scala.deriving.Mirror.ProductOf[T] => m.fromProduct(_)
+        case ev: (T <:< AnyNamedTuple)             => Tuple.fromProduct(_).asInstanceOf[T]
+      }
+    )
+
+enum ColSchema[T]:
+  case Str extends ColSchema[String]
+
+object ColSchema:
+  given StrIsGiven: ColSchema[String] = ColSchema.Str
+
 sealed trait InsertQuery[T]:
+  import InsertQuery.*
+
+  private final def findTable: Table[T] = this match
+    case InsertTo(t)         => t
+    case Values(inner, _, _) => inner.findTable
+
+  lazy val table: Table[T] = findTable
+
   // TODO: add constraints
   def values[N <: Tuple: {HasNames as names}, V <: Tuple: ExprLiftable.Id](
       v: NamedTuple[N, V]
   ): InsertQuery[T] = InsertQuery.Values(this, names, v.toTuple.lift)
 
 object InsertQuery:
-  case class InsertTo[T](table: Table[T]) extends InsertQuery[T]
+  case class InsertTo[T](schema: Table[T]) extends InsertQuery[T]
   case class Values[T, N <: Tuple, V <: Tuple](
       base: InsertQuery[T],
       names: HasNames[N],
@@ -29,15 +67,20 @@ object InsertQuery:
   ) extends InsertQuery[T]
 
 sealed trait DeleteQuery[T]:
-  def table: Table[T]
+  import DeleteQuery.*
+
+  private final def findTable: Table[T] = this match
+    case DeleteFrom(t)      => t
+    case Filtered(inner, _) => inner.findTable
+
+  lazy val table: Table[T] = findTable
+
   def filter(f: Expr[T] => Expr[Boolean]): DeleteQuery[T] =
-    val t = table
-    Filtered(t, this, f(Expr.Ref(t.RefId())))
+    Filtered(this, f(Expr.Ref(Expr.RefId(table))))
 
 object DeleteQuery:
-  case class DeleteFrom[T](table: Table[T]) extends DeleteQuery[T]
-  case class Filtered[T](table: Table[T], base: DeleteQuery[T], filtered: Expr[Boolean])
-      extends DeleteQuery[T]
+  case class DeleteFrom[T](schema: Table[T]) extends DeleteQuery[T]
+  case class Filtered[T](inner: DeleteQuery[T], filtered: Expr[Boolean]) extends DeleteQuery[T]
 
 sealed trait SelectQuery[T, IsScalar <: Boolean]
 object SelectQuery:
@@ -86,7 +129,10 @@ sealed trait Expr[T] extends Selectable:
   def ===[U: ExprLiftable.As[T]](that: U): Expr[Boolean] = Expr.Eq(this, that.lift)
 
 object Expr:
-  case class Ref[T](id: Table[T]#RefId) extends Expr[T]
+  final class RefId[T](val table: Table[T]):
+    override def toString(): String =
+      s"${table.name}.id(0x${Integer.toHexString(hashCode()).take(5)})"
+  case class Ref[T](id: RefId[T]) extends Expr[T]
   case class Sel[X, T](expr: Expr[X], name: String) extends Expr[T]
   case class StrLit(value: String) extends Expr[String]
   case class Tup[T <: Tuple](values: Vector[Expr[?]]) extends Expr[T]
@@ -103,7 +149,9 @@ type RunResult[T, IsScalar] = IsScalar match
   case false => Seq[T]
 
 trait DB:
-  def run[T, IsScalar <: Boolean, Res](q: SelectQuery[T, IsScalar])(using
+  def run[T](q: InsertQuery[T]): T
+  def run[T](q: DeleteQuery[T]): Unit
+  def run[T, IsScalar <: Boolean: ValueOf, Res](q: SelectQuery[T, IsScalar])(using
       RunResult[T, IsScalar] =:= Res
   ): Res
 
