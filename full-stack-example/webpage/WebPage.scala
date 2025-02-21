c479a71747c5d03b4fb505a7ffb3abfd5d9f810f
Jamie Thompson <bishbashboshjt@gmail.com>
2025-02-21 19:20:55 +0100

    switch to laminar in webapp, delete client wrapper

--- full-stack-example/webpage/WebPage.scala
+++ full-stack-example/webpage/WebPage.scala
@@ -1,66 +1,90 @@
 package example
 
-import org.scalajs.dom.html.Element
 import org.scalajs.dom.document
-import org.scalajs.dom.html.*
+import scala.concurrent.ExecutionContext.Implicits.given
+import model.Note
 
-import DomHelper.*
+import com.raquo.laminar.api.L.*
+import serverlib.fetchhttp.Client
+import serverlib.httpservice.HttpService
 
-import scala.concurrent.ExecutionContext
-import scala.util.control.NonFatal
-import example.model.Note
+import upicklex.namedTuples.Macros.Implicits.given
+import utils.given
 
-object WebPage:
-  given ExecutionContext = ExecutionContext.global
-  val service = new HttpClient()
+val client = Client.ofEndpoints(
+  HttpService.endpoints[NoteService],
+  baseURI = "http://localhost:8080/"
+)
 
-  val titleInput = input()
-  val contentTextArea = textarea()
+def app: HtmlElement =
+  val notesVar = Var(Seq.empty[Note])
+  val deleteBus = EventBus[(id: String)]()
+  val saveClicks = EventBus[(title: String, content: String)]()
 
-  val saveButton = button("Create Note")
-  saveButton.onclick = _ =>
-    service
-      .createNote(titleInput.value, contentTextArea.value)
-      .map(addNote)
+  val deletionEvents = deleteBus.events
+    .flatMapSwitch: form =>
+      EventStream
+        .fromFuture(client.deleteNote.send(form))
+        .mapTo(form.id)
 
-  val form: Div = div(
-    titleInput,
-    contentTextArea,
-    saveButton
-  )
-  form.className = "note-form"
-
-  val appContainer: Div = div(
-    h1("My Notepad"),
-    form
-  )
-  appContainer.id = "app-container"
-
-  def addNote(note: Note): Unit =
-    val elem = div(
-      h2(note.title),
-      p(note.content)
+  val savedEvents = saveClicks.events
+    .flatMapSwitch(form =>
+      EventStream
+        .fromFuture(
+          client.createNote
+            .send((body = form))
+        )
     )
 
-    val deleteButton = button("Delete Note")
-    deleteButton.onclick = _ =>
-      service
-        .deleteNote(note.id)
-        .map(res => if res then appContainer.removeChild(elem))
+  def noteElem(note: Note) =
+    div(
+      className := "note",
+      h2(note.title),
+      p(note.content),
+      button(
+        "Delete Note",
+        onClick.mapTo((id = note.id)) --> deleteBus.writer
+      ),
+      deletionEvents --> notesVar.updater[String]((ns, id) =>
+        ns.filterNot(_.id == id)
+      )
+    )
 
-    elem.appendChild(deleteButton)
-    elem.className = "note"
-    appContainer.appendChild(elem)
+  def form() =
+    val titleInput = Var("")
+    val contentTextArea = Var("")
+    div(
+      className := "note-form",
+      input(
+        placeholder := "Title",
+        onChange.mapToValue --> titleInput.writer
+      ),
+      textArea(
+        placeholder := "Content",
+        onChange.mapToValue --> contentTextArea.writer
+      ),
+      button(
+        "Create Note",
+        onClick.mapToUnit.compose(
+          _.sample(titleInput.signal, contentTextArea.signal)
+        ) --> saveClicks.writer
+      ),
+      savedEvents --> notesVar.updater[Note](_ :+ _)
+    )
+  end form
 
-  @main def start: Unit =
-    document.body.appendChild(appContainer)
-    println("fetching notes")
-    val ns = service.getAllNotes()
-    ns.andThen {
-      case scala.util.Success(notes) =>
-        notes.foreach(addNote)
-      case scala.util.Failure(e) =>
-        println(s"Failed to fetch notes: $e")
-    }
+  div(
+    idAttr := "app-container",
+    h1("My Notepad"),
+    form(),
+    children <-- notesVar.signal.map(_.map(noteElem)),
+    onMountBind(_ =>
+      EventStream.fromFuture(
+        client.getAllNotes.send(NamedTuple.Empty)
+      ) --> notesVar.writer
+    )
+  )
+end app
 
-    // for notes <- service.getAllNotes(); note <- notes do addNote(note)
+@main def start: Unit =
+  renderOnDomContentLoaded(document.body, app)
